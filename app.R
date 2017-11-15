3236db39f884217e52ec98bd6fc12db47fd2e181
zankuralt <zan.kuralt@gmail.com>
2017-11-15 15:32:05 +0100

    added files

---
+++ app.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+library(leaflet)
+
+header <- dashboardHeader(title = "geodev")
+
+sidebar <- dashboardSidebar(collapsed = TRUE,
+  sidebarMenu(
+  actionButton(inputId = "removeMarkers", label = "Remove markers", icon = icon("remove"), width = "85%")
+  )
+)
+
+body <- dashboardBody(
+  # useShinyjs(),
+  tags$style(type = "text/css", "#map {height: 100% !important;}"),
+  
+  div(class="outer",
+      tags$head(
+        
+        # Include  custom CSS
+        includeCSS("styles.css")
+      ),
+      leafletOutput("map")
+  ) 
+)
+
+ui <- dashboardPage(header, sidebar, body)
+
+server <- function(input, output) {
+  
+  output$map <- renderLeaflet({
+    leaflet() %>% 
+      addTiles() %>%
+      # addProviderTiles(provider = providers$OpenStreetMap) %>%
+      setView(lng = 14.815333, lat = 46.119944, zoom = 7) %>%
+      addMiniMap(
+        tiles = providers$OpenStreetMap,
+        toggleDisplay = TRUE) %>%
+      addEasyButton(easyButton(
+        icon="fa-crosshairs", title="Locate Me",
+        onClick=JS("function(btn, map){ map.locate({setView: true}); }")))
+  })
+  
+  observeEvent(input$map_click, {
+    click <- input$map_click
+    # alert("Click registered. Marker will be added.")
+    pnt <- data.frame(lat = click$lat, lng = click$lng)
+    leafletProxy(mapId = "map", data = pnt) %>%
+      addCircleMarkers(group = "markers", popup = paste(paste("lat:", click$lat, "lng:", click$lng, sep = " ")))
+  })
+
+  observeEvent(input$removeMarkers, {
+    leafletProxy(mapId = "map") %>%
+      clearMarkers()
+  })
+  
+}
+
+shinyApp(ui, server)
